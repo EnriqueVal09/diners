7b4afe402f41df73c8722ba7b1f4e0d35469e2ca
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-04-28 21:02:48 -0600

    Diner and QRCode models (including admin models) were created

--- apps/diners/admin.py
+++ apps/diners/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Diner, QRCode
+
+@admin.register(Diner)
+class DinerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'theme_color', 'created_at')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(QRCode)
+class QRCodeAdmin(admin.ModelAdmin):
+    list_display = ('diner', 'url')
+    search_fields = ('diner__name',)
+
