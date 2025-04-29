85a633095b57321bd1686d8f707d82078702eb22
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-04-28 21:32:46 -0600

    QRCodeInlineAdmin instead of QRCodeAdmin

--- apps/diners/admin.py
+++ apps/diners/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 
 from .models import Diner, QRCode
 
+class QRCodeInlineAdmin(admin.TabularInline):
+    model = QRCode
+    extra = 0
+
 @admin.register(Diner)
 class DinerAdmin(admin.ModelAdmin):
     list_display = ('name', 'theme_color', 'created_at')
     search_fields = ('name',)
     ordering = ('name',)
+    inlines = [QRCodeInlineAdmin]
 
-@admin.register(QRCode)
-class QRCodeAdmin(admin.ModelAdmin):
-    list_display = ('diner', 'qr_url')
-    search_fields = ('diner__name',)
 
