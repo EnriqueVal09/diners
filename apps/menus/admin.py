fd51d38be3ac6aa56f2a44c617e25d0a27b37081
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-04-28 21:48:42 -0600

    Category, Meal and MealOption models (including admin models) were created

--- apps/menus/admin.py
+++ apps/menus/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Meal, MealOption
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'order')
+    search_fields = ('name',)
+    list_filter = ('parent',)
+
+class MealOptionInlineAdmin(admin.TabularInline):
+    model = MealOption
+    extra = 0
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    inlines = [MealOptionInlineAdmin]
--- apps/menus/models.py
+++ apps/menus/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+from apps.diners.models import Diner
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    diner = models.ForeignKey(Diner, on_delete=models.CASCADE, related_name="categories", verbose_name="Restaurante")
+    name = models.CharField("Nombre de la categoría", max_length=100)
+    order = models.IntegerField("Orden de visualización", default=0)
+    parent_category = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="subcategories",
+        verbose_name="Categoría padre"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Meal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="meals", verbose_name="Categoría")
+    name = models.CharField("Nombre del platillo", max_length=100)
+    description = models.TextField("Descripción")
+    price = models.DecimalField("Precio", max_digits=8, decimal_places=2)
+    available = models.BooleanField("Disponible", default=True)
+    image_url = models.URLField("URL de imagen del platillo", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MealOption(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="options", verbose_name="Platillo")
+    name = models.CharField("Nombre de la opción", max_length=100)
+    extra_price = models.DecimalField("Precio extra", max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} (+{self.extra_price})"
