bbc8e393055486f4eb0d3c060327eb50b473fa8c
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-04-30 17:19:34 -0600

    MealByCategoryListView was implemented

--- apps/menus/urls.py
+++ apps/menus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, MealByCategoryListView
 
 urlpatterns = [
-    path("", CategoryListView.as_view(), name="all-meal-list"),
+    path("", CategoryListView.as_view(), name="full-menu"),
+    path('category/<uuid:category_id>/', MealByCategoryListView.as_view(), name='meals_by_category'),
 ]
--- apps/menus/views.py
+++ apps/menus/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
 from django.views.generic import ListView
+
 from .models import Meal, Category
 from apps.diners.models import Diner
 
 class CategoryListView(ListView):
     model = Category
-    template_name = "meals/all_meal_list.html"
+    template_name = "meals/full_menu.html"
     context_object_name = "categories"
 
     def get_queryset(self):
@@ -20,4 +20,21 @@ class CategoryListView(ListView):
             'subcategories__subcategories__meals'
         ).order_by('order', 'name')
 
+class MealByCategoryListView(ListView):
+    model = Meal
+    template_name = "meals/meals_by_category.html"
+    context_object_name = "meals"
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        return Meal.objects.filter(
+            category__id=category_id,
+            available=True
+        ).order_by("name").prefetch_related("options")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Agregamos la categoría para mostrar su nombre en el template
+        context['category'] = Category.objects.get(id=self.kwargs.get('category_id'))
+        return context
 
