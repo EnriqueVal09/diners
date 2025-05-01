73c7affa3abdb22e2648bd83fb2b69eaf7e09d04
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-04-30 18:08:31 -0600

    Improving the way of listing categories in the navbar

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,9 +2,36 @@ from apps.diners.models import Diner
 from apps.menus.models import Category
 
 def diner_context(request):
-    diner = Diner.objects.first()
+    try:
+        diner = Diner.objects.first()
+    except:
+        diner = None
     return {"diner": diner}
 
 def category_context(request):
-    categories = Category.objects.all()
-    return {"categories": categories}
+    try:
+        categories = Category.objects.all()
+        bebidas_category = categories.filter(name="Bebidas").first()
+
+        if bebidas_category:
+            # Si sí existe la categoría "Bebidas"
+            menu_categories = categories.exclude(
+                id=bebidas_category.id
+            ).exclude(
+                parent_category=bebidas_category
+            )
+            bebida_categories = categories.filter(parent_category=bebidas_category)
+        else:
+            # Si NO existe la categoría "Bebidas"
+            menu_categories = categories
+            bebida_categories = []
+
+    except:
+        menu_categories = []
+        bebida_categories = []
+
+    return {
+        "menu_categories": menu_categories,
+        "bebida_categories": bebida_categories,
+    }
+
